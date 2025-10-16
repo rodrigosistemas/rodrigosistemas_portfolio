50d2d7b5c5e2715847d1a007e448e323db786572
rodrigosistemas <rodhernandez.code@gmail.com>
2025-10-15 22:06:12 -0500

    Se toman imagenes de manera local en el carousel

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -9,7 +9,7 @@ import { NzCarouselModule } from 'ng-zorro-antd/carousel';
   styleUrl: './carousel.component.css'
 })
 export class NzCarouselAutoplayComponent {
-  arrayOfImgUrls = ['https://certifications-rodrigosistemas.s3.us-east-2.amazonaws.com/CCNAv7+Introduction+to+Networks.jpg', 'https://certifications-rodrigosistemas.s3.us-east-2.amazonaws.com/CCNA+Fundamentos+de+Conmutaci%C3%B3n+y+Enrutamiento+y+Redes+Inal%C3%A1mbricas.jpg', 'https://certifications-rodrigosistemas.s3.us-east-2.amazonaws.com/Cisco+Junior+Cybersecurity+Analyst.jpg', 'https://certifications-rodrigosistemas.s3.us-east-2.amazonaws.com/Cisco+Ethical+Hacker.jpg' ];
+  arrayOfImgUrls = ['images/badges/CCNAv7IntroductiontoNetworks.jpg', 'images/badges/CCNAFundamentosRedes.jpg', 'images/badges/CiscoEthicalHacker.jpg', 'images/badges/CiscoJuniorCybersecurityAnalyst.jpg'];
 }
 
 
